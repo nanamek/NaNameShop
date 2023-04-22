a5bb65ff26a5591e9975ed0307e1719c3bafe401
nanamek <45305292+nanamek@users.noreply.github.com>
2023-04-22 17:11:04 +0700

    First upload

---
+++ nanameshop/boardgames/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=300)
+    description = models.TextField()
+    suitable = models.CharField(max_length=4, default='7+')
+    image = models.ImageField(upload_to='product_images/', null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
